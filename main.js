ca27ba2e936b20d164f95564d99b58fa466c8b6c
Lea225 <132575354+Lea225@users.noreply.github.com>
2024-06-12 11:52:47 +0100

    Update main.js

--- main.js
+++ main.js
@@ -153,6 +153,31 @@ document.addEventListener('DOMContentLoaded', () => {
         cart.classList.toggle('show');
     });
 
+    const faqItems = document.querySelectorAll('.faq-item');
+  
+    faqItems.forEach((item) => {
+      const question = item.querySelector('.question');
+      const iconPlus = item.querySelector('.icon-plus');
+      const iconMinus = item.querySelector('.icon-minus');
+      const answer = item.querySelector('.answer');
+  
+      iconPlus.addEventListener('click', function () {
+        // Afficher la réponse
+        answer.style.display = 'block';
+        // Changer les icônes
+        iconPlus.style.display = 'none';
+        iconMinus.style.display = 'inline-block';
+      });
+  
+      iconMinus.addEventListener('click', function () {
+        // Cacher la réponse
+        answer.style.display = 'none';
+        // Changer les icônes
+        iconPlus.style.display = 'inline-block';
+        iconMinus.style.display = 'none';
+      });
+    });
+
     const searchInput = document.querySelector('input[type="search"]');
 
     searchInput.addEventListener('input', () => {
