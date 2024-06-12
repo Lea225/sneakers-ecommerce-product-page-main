afc40003bf980a43c1eca8c4e032e08cbc7580e5
Lea225 <johndoe@example.com>
2024-06-12 12:02:54 +0100

    second commit

--- main.js
+++ main.js
@@ -146,6 +146,31 @@ document.addEventListener('DOMContentLoaded', () => {
         navigation.style.display = "none";
     });
 
+    const faqItems = document.querySelectorAll('.faq-item');
+  
+    faqItems.forEach((item) => {
+      const question = item.querySelector('.question');
+      const iconPlus = item.querySelector('.icon-plus');
+      const iconMinus = item.querySelector('.icon-minus');
+      const answer = item.querySelector('.answer');
+  
+      iconPlus.addEventListener('click', function () {
+        // Afficher la réponse
+        answer.style.display = 'block';
+        // Changer les icônes
+        iconPlus.style.display = 'none';
+        iconMinus.style.display = 'inline-block';
+      });
+  
+      iconMinus.addEventListener('click', function () {
+        // Cacher la réponse
+        answer.style.display = 'none';
+        // Changer les icônes
+        iconPlus.style.display = 'inline-block';
+        iconMinus.style.display = 'none';
+      });
+    });
+
     const cartIcon = document.querySelector('.header-image-cart-container');
     const cart = document.querySelector('.cart');
 
